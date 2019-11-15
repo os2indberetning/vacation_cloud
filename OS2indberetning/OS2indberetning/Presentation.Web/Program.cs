13bee1e210d103013695379d96f753b0806ab0f8
Peter Søgaard <pso@digital-identity.dk>
2019-11-15 09:48:17 +0100

    changed logging to Serilog

--- OS2indberetning/OS2indberetning/Presentation.Web/Program.cs
+++ OS2indberetning/OS2indberetning/Presentation.Web/Program.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 using Serilog;
+using System.IO;
 
 namespace Presentation.Web
 {
@@ -10,19 +10,22 @@ namespace Presentation.Web
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(config)
+                .CreateLogger();
+
             CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseConfiguration(new ConfigurationBuilder().AddJsonFile("appsettings.json").AddEnvironmentVariables().Build())
-                .ConfigureLogging((hostingContext, logging) =>
-                {
-                    logging.ClearProviders();
-                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
-                    logging.AddConsole();
-                    logging.AddDebug();
-                })
+                .UseSerilog()
                 .UseStartup<Startup>();
     }
 }
