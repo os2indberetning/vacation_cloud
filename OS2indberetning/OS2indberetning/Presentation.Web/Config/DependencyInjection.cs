04ec667303374366fce61d5cb91a7e657891ddbf
Peter Søgaard <pso@digital-identity.dk>
2019-11-20 14:10:52 +0100

    typo

--- OS2indberetning/OS2indberetning/Presentation.Web/Config/DependencyInjection.cs
+++ OS2indberetning/OS2indberetning/Presentation.Web/Config/DependencyInjection.cs
@@ -26,7 +26,8 @@ namespace Presentation.Web.Config
                 "Server=" + configuration["Database:Server"] + 
                 ";Database=" + configuration["Database:Database"] + 
                 ";Uid=" + configuration["Database:Uid"] + 
-                ";Pwd=" + configuration["Database:Pwd"] + ";", mysqlOptions => mysqlOptions.ServerVersion(new System.Version(5, 6, 30), ServerType.MySql))); ;
+                ";Pwd=" + configuration["Database:Pwd"] + ";", 
+                    mysqlOptions => mysqlOptions.ServerVersion(new System.Version(5, 6, 30), ServerType.MySql)));
             services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
             services.AddScoped<IPersonService, PersonService>();
             services.AddScoped<IMobileTokenService,MobileTokenService>();
