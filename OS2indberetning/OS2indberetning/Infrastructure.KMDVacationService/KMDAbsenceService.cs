dc06cef3aed836e3575f28f1c1fa57a67e21d4b5
Peter Søgaard <pso@digital-identity.dk>
2019-11-27 10:52:15 +0100

    added warning message when kmd service is disabled

--- OS2indberetning/OS2indberetning/Infrastructure.KMDVacationService/KMDAbsenceService.cs
+++ OS2indberetning/OS2indberetning/Infrastructure.KMDVacationService/KMDAbsenceService.cs
@@ -21,89 +21,94 @@ namespace Infrastructure.KMDVacationService
         }
         public void SetAbsence(IList<KMDAbsenceReport> absenceReports)
         {
-            if (Boolean.Parse(config["KMDVacationService:ServiceEnabled"]))
+            if (!Boolean.Parse(config["KMDVacationService:ServiceEnabled"]))
             {
+                logger.LogWarning("KMDVacationService is disabled. Not setting any absenceReports");
+                return;
+            }
 
-                var webService = new SetAbsenceAttendance.SetAbsenceAttendance_OS_SIClient();
-                var certificate = new X509Certificate2(config["KMDVacationService:CertificateFilename"], config["KMDVacationService:CertificatePassword"]);
-                ((BasicHttpBinding)webService.Endpoint.Binding).Security.Transport.ClientCredentialType = HttpClientCredentialType.Certificate;
-                webService.Endpoint.Address = new EndpointAddress(config["KMDVacationService:SetAbsenceAttendanceEndpoint"]);
-                webService.Endpoint.EndpointBehaviors.Add(new LoggingBehavior(logger));
+            var webService = new SetAbsenceAttendance.SetAbsenceAttendance_OS_SIClient();
+            var certificate = new X509Certificate2(config["KMDVacationService:CertificateFilename"], config["KMDVacationService:CertificatePassword"]);
+            ((BasicHttpBinding)webService.Endpoint.Binding).Security.Transport.ClientCredentialType = HttpClientCredentialType.Certificate;
+            webService.Endpoint.Address = new EndpointAddress(config["KMDVacationService:SetAbsenceAttendanceEndpoint"]);
+            webService.Endpoint.EndpointBehaviors.Add(new LoggingBehavior(logger));
 
-                webService.ClientCredentials.ClientCertificate.Certificate = certificate;
+            webService.ClientCredentials.ClientCertificate.Certificate = certificate;
 
-                foreach (var report in absenceReports)
+            foreach (var report in absenceReports)
+            {
+                var request = new SetAbsenceAttendance.SetAbsenceAttendanceRequest();
+                request.StartDate = report.StartDate.ToString("yyyy-MM-dd");
+                request.StartTime = report.StartTime?.ToString(@"hh\:mm\:ss");
+                request.EndDate = report.EndDate.ToString("yyyy-MM-dd");
+                request.EndTime = report.EndTime?.ToString(@"hh\:mm\:ss");
+                request.OriginalStartDate = report.OldStartDate?.ToString("yyyy-MM-dd");
+                request.OriginalStartTime = report.OldEndTime?.ToString(@"hh\:mm\:ss");
+                request.OriginalEndDate = report.OldEndDate?.ToString("yyyy-MM-dd");
+                request.OriginalEndTime = report.OldEndTime?.ToString(@"hh\:mm\:ss");
+                request.PersonnelNumber = report.EmploymentId.ToString();
+                request.Operation = report.KmdAbsenceOperation.AsString();
+                request.AdditionalData = report.ExtraData;
+
+                switch (report.Type)
                 {
-                    var request = new SetAbsenceAttendance.SetAbsenceAttendanceRequest();
-                    request.StartDate = report.StartDate.ToString("yyyy-MM-dd");
-                    request.StartTime = report.StartTime?.ToString(@"hh\:mm\:ss");
-                    request.EndDate = report.EndDate.ToString("yyyy-MM-dd");
-                    request.EndTime = report.EndTime?.ToString(@"hh\:mm\:ss");
-                    request.OriginalStartDate = report.OldStartDate?.ToString("yyyy-MM-dd");
-                    request.OriginalStartTime = report.OldEndTime?.ToString(@"hh\:mm\:ss");
-                    request.OriginalEndDate = report.OldEndDate?.ToString("yyyy-MM-dd");
-                    request.OriginalEndTime = report.OldEndTime?.ToString(@"hh\:mm\:ss");
-                    request.PersonnelNumber = report.EmploymentId.ToString();
-                    request.Operation = report.KmdAbsenceOperation.AsString();
-                    request.AdditionalData = report.ExtraData;
-
-                    switch (report.Type)
-                    {
-                        case VacationType.Regular:
-                            request.AbsenceAttendanceType = "FE";
-                            request.OriginalAbsenceAttendanceType = "FE";
-                            break;
-                        case VacationType.SixthVacationWeek:
-                            request.AbsenceAttendanceType = "6F";
-                            request.OriginalAbsenceAttendanceType = "6F";
-                            break;
-                        case VacationType.Senior:
-                            request.AbsenceAttendanceType = "SO";
-                            request.OriginalAbsenceAttendanceType = "SO";
-                            break;
-                        case VacationType.Care:
-                            request.AbsenceAttendanceType = "OS";
-                            request.OriginalAbsenceAttendanceType = "OS";
-                            break;
-                        default:
-                            throw new NotSupportedException();
-                    }
-                    var response = webService.SetAbsenceAttendance_OS_SI(request);
-
-                    // If TYPE is empty, it succeeded
-                    if (response.ReturnStatus.StatusType == "" || response.ReturnStatus.StatusType == "S") continue;
-
-                    // Error occurred, cast exception containing error message.
-                    throw new KMDSetAbsenceFailedException(response.ReturnStatus.MessageText, request);
+                    case VacationType.Regular:
+                        request.AbsenceAttendanceType = "FE";
+                        request.OriginalAbsenceAttendanceType = "FE";
+                        break;
+                    case VacationType.SixthVacationWeek:
+                        request.AbsenceAttendanceType = "6F";
+                        request.OriginalAbsenceAttendanceType = "6F";
+                        break;
+                    case VacationType.Senior:
+                        request.AbsenceAttendanceType = "SO";
+                        request.OriginalAbsenceAttendanceType = "SO";
+                        break;
+                    case VacationType.Care:
+                        request.AbsenceAttendanceType = "OS";
+                        request.OriginalAbsenceAttendanceType = "OS";
+                        break;
+                    default:
+                        throw new NotSupportedException();
                 }
+                var response = webService.SetAbsenceAttendance_OS_SI(request);
+
+                // If TYPE is empty, it succeeded
+                if (response.ReturnStatus.StatusType == "" || response.ReturnStatus.StatusType == "S") continue;
+
+                // Error occurred, cast exception containing error message.
+                throw new KMDSetAbsenceFailedException(response.ReturnStatus.MessageText, request);
             }
         }
 
         public List<Child> GetChildren(Employment employment)
         {
             var children = new List<Child>();
-            if (Boolean.Parse(config["KMDVacationService:ServiceEnabled"]))
+            if (!Boolean.Parse(config["KMDVacationService:ServiceEnabled"]))
             {
-                var webService = new GetChildren.GetChildren_OS_SIClient();
-                var certificate = new X509Certificate2(config["KMDVacationService:CertificateFilename"], config["KMDVacationService:CertificatePassword"]);
-                webService.ClientCredentials.ClientCertificate.Certificate = certificate;
-                ((BasicHttpBinding)webService.Endpoint.Binding).Security.Transport.ClientCredentialType = HttpClientCredentialType.Certificate;
-                webService.Endpoint.Address = new EndpointAddress(config["KMDVacationService:GetChildrenEndpoint"]);
-                webService.Endpoint.EndpointBehaviors.Add(new LoggingBehavior(logger));
+                logger.LogWarning("KMDVacationService is disabled. Returning empty list of children");
+                return children;
+            }
 
-                var request = new GetChildren.GetChildrenRequest();
-                request.PersonnelNumber = employment.EmploymentId.ToString();
+            var webService = new GetChildren.GetChildren_OS_SIClient();
+            var certificate = new X509Certificate2(config["KMDVacationService:CertificateFilename"], config["KMDVacationService:CertificatePassword"]);
+            webService.ClientCredentials.ClientCertificate.Certificate = certificate;
+            ((BasicHttpBinding)webService.Endpoint.Binding).Security.Transport.ClientCredentialType = HttpClientCredentialType.Certificate;
+            webService.Endpoint.Address = new EndpointAddress(config["KMDVacationService:GetChildrenEndpoint"]);
+            webService.Endpoint.EndpointBehaviors.Add(new LoggingBehavior(logger));
 
-                var response = webService.GetChildren_OS_SI(request);
+            var request = new GetChildren.GetChildrenRequest();
+            request.PersonnelNumber = employment.EmploymentId.ToString();
 
-                foreach (var kmdChild in response.Child)
-                {
-                    var child = new Child();
-                    child.Id = int.Parse(kmdChild.ChildNumber);
-                    child.FirstName = String.IsNullOrWhiteSpace(kmdChild.FirstName) ? "Ukendt navn" : kmdChild.FirstName;
-                    child.LastName = kmdChild.LastName ?? "";
-                    children.Add(child);
-                }
+            var response = webService.GetChildren_OS_SI(request);
+
+            foreach (var kmdChild in response.Child)
+            {
+                var child = new Child();
+                child.Id = int.Parse(kmdChild.ChildNumber);
+                child.FirstName = String.IsNullOrWhiteSpace(kmdChild.FirstName) ? "Ukendt navn" : kmdChild.FirstName;
+                child.LastName = kmdChild.LastName ?? "";
+                children.Add(child);
             }
             return children;
         }
