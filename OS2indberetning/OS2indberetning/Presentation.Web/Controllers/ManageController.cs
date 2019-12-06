0a85d46afc6b42b7b06019401c613f745538184a
Peter Søgaard <pso@digital-identity.dk>
2019-12-06 11:57:17 +0100

    added health endpoint

---
+++ OS2indberetning/OS2indberetning/Presentation.Web/Controllers/ManageController.cs
@@ -0,0 +1,30 @@
+﻿using Core.DomainModel;
+using Core.DomainServices;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace Presentation.Web.Controllers.API
+{
+    [ApiController]
+    public class ManageController : ControllerBase
+    {
+        private IGenericRepository<OrgUnit> orgUnitRepo;
+        public ManageController(IGenericRepository<OrgUnit> orgUnitRepo)
+        {
+            this.orgUnitRepo = orgUnitRepo;
+        }
+
+        [Route("manage/health")]
+        public ActionResult Health()
+        {
+            // a simple health endpoint check that also fails if there is no connection to database
+            if (!orgUnitRepo.AsNoTracking().Any(o => o.HasAccessToVacation))
+            {
+                throw new Exception("Health endpoint reports no OrgUits with access to Vacation");
+            }
+
+            return Ok();
+        }
+    }
+}
